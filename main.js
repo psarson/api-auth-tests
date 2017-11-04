56260f4be5ca4c89feb63c086b4f30f1a95694b1
Patrick Sarson <sarsonmedia@gmail.com>
2017-11-03 20:09:27 -0400

    git outta here

---
+++ main.js
@@ -0,0 +1,17 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+function signIn() {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        var token = result.credential.accessToken; 
+        var user = result.user; 
+        console.log(user)
+        
+    }).catch(function (error) {          
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;    
+    }); 
+};  
+
+
